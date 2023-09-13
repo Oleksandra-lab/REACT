f36750b169e017acb1b055f757473b8b2381ee9b
Oleksandra-lab <127051632+Oleksandra-lab@users.noreply.github.com>
2023-09-13 22:35:42 +0300

    first component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import data from './data.json'
 // import './index.css';
 
-const root = document.getElementById('root')
+// const paragraph = React.createElement('p', {id: 'test-P-id', children: ['I am P']})
 
-const div = React.createElement('div')
+// const p = <p id='test-P-id'>I am P</p>
 
-ReactDOM.createRoot(root).render(div)
+// const div = React.createElement('div', { name: 'main', id: 'test-id', className: 'test-class', children: p,}, )
+// const div = <div  name='main' id='test-id' className='test-class'>{p}</div>
+
+const Head = () => {
+    return <p> I am Head</p>
+}
 
 
+const Card = () => {
+    return data.map(photo => {
+        return(
+            <div key={photo.id} className="card mx-auto my-2" style={{width: '18rem'}}>
+<img src={photo.url} className="card-img-top" alt={photo.title}/>
+<div className="card-body">
+  <h5 className="card-title">Card title: {photo.title}</h5>
+  {/* {head()} */}
+  <Head />
+  <a href="htpp" className="btn btn-primary">Go somewhere</a>
+</div>
+</div>
+        )
+    })
+}
 
-
+ReactDOM.createRoot(document.getElementById('root')).render(<Card />)
 
 
 
